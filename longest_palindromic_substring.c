4e1f27cff2a97ad2a032481825116c9058683a33
Quan <quan@quan.com>
2018-10-28 00:34:39 -0400

    Add longest palindromic skeleton code

---
+++ longest_palindromic_substring.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+const int MAX_LENGTH = 1000;
+
+char longest_string[MAX_LENGTH];
+char temp_string[MAX_LENGTH];
+
+char* palindrome() {
+	return 0;
+}
+
+char* longestPalindrome(char* s) {
+	return s;
+}
+
+int main() {
+	return 0;
+}
